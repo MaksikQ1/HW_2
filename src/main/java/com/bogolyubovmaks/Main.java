e20f351b9187bc49fe60bd4b245e866839071290
Maksim Bogolyubov <bogolyubovmaks@gmail.com>
2025-03-02 17:55:07 +0500

    Конфликт1

--- src/main/java/com/bogolyubovmaks/Main.java
+++ src/main/java/com/bogolyubovmaks/Main.java
@@ -7,7 +7,7 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
 
-        System.out.print("есть конфликт");
+        System.out.print("есть конфликт!");
 
 
         for (int i = 1; i <= 5; i++) {
