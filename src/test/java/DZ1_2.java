7365cc8806f092b8963fa54494123608c403ea85
Maksim Bogolyubov <bogolyubovmaks@gmail.com>
2025-03-02 15:38:51 +0500

    Dolil ostalnoe

--- src/test/java/DZ1_2.java
+++ src/test/java/DZ1_2.java
@@ -4,12 +4,13 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-public class SearchTests {
+public class Dz1_2 {
     @Test
     void successfulSearchTest() {
-        open("https://ya.ru/");
-        $("[id=text]").setValue("selenide").pressEnter();
-        $("[id=search-result]").shouldHave(text("ru.selenide.org"));
+        open("https://auth.niffler.qa.guru");
+        $("[name=username]").setValue("usr");
+        $("[name=password]").setValue("psw").pressEnter();
+
+        $("[id=spendings]").shouldHave(text("Kat1"));
     }
 }
-//3
