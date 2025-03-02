e8bfadc270b85ff495ddcdff374a171fdd49abc3
MaksikQ1 <bogolyubovmaks@gmail.com>
2025-03-02 17:49:01 +0500

    Update Main.java

--- src/main/java/com/bogolyubovmaks/Main.java
+++ src/main/java/com/bogolyubovmaks/Main.java
@@ -7,7 +7,7 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
 
-        System.out.print("НЕТ КОНФЛИКАТА");
+        System.out.print("НЕТ КОНФЛИКАТА!!");
 
 
         for (int i = 1; i <= 5; i++) {
